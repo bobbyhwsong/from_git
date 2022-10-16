d043a9c771fe856ef3bdc996752e9c776e181821
rfrbby7 <rotto95@snu.ac.kr>
2022-10-17 03:57:00 +0900

    ss

--- FlaskScrapyBeautifulsoup/tutorial/tutorial/items.py
+++ FlaskScrapyBeautifulsoup/tutorial/tutorial/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class PracticeItem(scrapy.Item):
+class TutorialItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
--- FlaskScrapyBeautifulsoup/tutorial/tutorial/settings.py
+++ FlaskScrapyBeautifulsoup/tutorial/tutorial/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for practice project
+# Scrapy settings for tutorial project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,17 +7,17 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'practice'
+BOT_NAME = 'tutorial'
 
-SPIDER_MODULES = ['practice.spiders']
-NEWSPIDER_MODULE = 'practice.spiders'
+SPIDER_MODULES = ['tutorial.spiders']
+NEWSPIDER_MODULE = 'tutorial.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'practice (+http://www.yourdomain.com)'
+#USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False #정책을 무시하기 위함
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -45,13 +45,13 @@ ROBOTSTXT_OBEY = False #정책을 무시하기 위함
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'practice.middlewares.PracticeSpiderMiddleware': 543,
+#    'tutorial.middlewares.TutorialSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'practice.middlewares.PracticeDownloaderMiddleware': 543,
+#    'tutorial.middlewares.TutorialDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = False #정책을 무시하기 위함
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'practice.pipelines.PracticePipeline': 300,
+#    'tutorial.pipelines.TutorialPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
